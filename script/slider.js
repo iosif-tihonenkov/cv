0a6eb9e3747b0183e4dfe3480bacc13c2703259e
iosif-tihonenkov <iosif.tihonenkov@gmail.com>
2022-05-11 08:03:46 +0300

    повесил клик назад

--- script/slider.js
+++ script/slider.js
@@ -1,9 +1,18 @@
+let images = [
+    'img/1.png',
+    'img/2.png',
+    'img/3.png',
+    'img/4.png',
+    'img/5.png'    
+];
+
+let num = 0;
+
 function backImg() {
-    let images = [
-        'img/1.png',
-        'img/2.png',
-        'img/3.png',
-        'img/4.png',
-        'img/5.png'    
-    ]
+    let slider = document.getElementById('slider');
+    num--;
+    if (num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
 }
